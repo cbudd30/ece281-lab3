30a2aa08a527fef78265b493ebb62ccc8411b211
cbudd30 <c27caitlyn.budd@afacademy.af.edu>
2025-04-02 14:40:39 -0600

    work in progress

--- src/thunderbird_fsm_tb.vhd
+++ src/thunderbird_fsm_tb.vhd
@@ -57,27 +57,102 @@ end thunderbird_fsm_tb;
 architecture test_bench of thunderbird_fsm_tb is 
 	
 	component thunderbird_fsm is 
---	  port(
-		
---	  );
+	  port (
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(2 downto 0);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+    );
 	end component thunderbird_fsm;
 
 	-- test I/O signals
+	signal w_leftin, w_rightin : std_logic := '0';
+	signal w_reset : std_logic := '0';
+	signal w_clk : std_logic := '0';
 	
+	--Outputs
+	signal w_left : std_logic_vector(2 downto 0); -- RYG one-hot
+	signal w_right : std_logic_vector(2 downto 0) ; -- RYG one-hot
+		
 	-- constants
+	constant k_clk_period : time := 10 ns;
+	
+	
 	
 	
 begin
 	-- PORT MAPS ----------------------------------------
+	 uut: thunderbird_fsm port map (
+          i_left => w_leftin,
+          i_right => w_rightin,
+          i_reset => w_reset,
+          i_clk => w_clk,
+          o_lights_L => w_left,
+          o_lights_R => w_right
+        );
 	
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
+    clk_proc : process
+	begin
+		w_clk <= '0';
+        wait for k_clk_period/2;
+		w_clk <= '1';
+		wait for k_clk_period/2;
+	end process;
     
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
+	sim_proc: process
+	begin
+		-- sequential timing		
+		w_reset <= '1';
+		wait for k_clk_period*1;
+		  assert w_left = "000" report "bad reset" severity failure;
+		  assert w_right = "000" report "bad reset" severity failure;
+		
+		w_reset <= '0';
+		wait for k_clk_period*1;
+		
+		-- left turn signal off
+		w_leftin <= '0'; wait for k_clk_period;
+          assert w_left = "000" report "left turn off" severity failure;
+        -- right turn signal off
+        w_rightin <= '0'; wait for k_clk_period;
+            assert w_right = "000" report "right turn off" severity failure;
+        
+        --left signal test
+        w_leftin <= '1'; wait for k_clk_period;
+            assert w_left = "001" report "left blinker on" severity failure;
+        wait for k_clk_period; -- time to go to next blink
+            assert w_left = "010" report "left blinker phase 2" severity failure;
+        wait for k_clk_period; -- time to go to final blink
+            assert w_left = "100" report "left blinker phase 3" severity failure;
+        
+        w_leftin <= '0';
+        
+        -- reset and test right blinker
+        w_reset <= '1';
+            wait for k_clk_period;
+        w_rightin <= '1'; wait for k_clk_period;
+            assert w_right = "001" report "right blinker phase 1" severity failure;
+        wait for k_clk_period;
+            assert w_right = "010" report "right blinker phase 2" severity failure;
+        wait for k_clk_period;
+            assert w_right = "100" report "right blinker phase 3" severity failure;
+            
+        --reset and test if both
+        w_reset <= '1';
+            wait for k_clk_period;
+        w_rightin <= '1'; w_leftin <= '1'; wait for k_clk_period;
+            assert w_right = "011" and w_left = "011" report "both flashing working" severity failure;
+            
+	
+		wait;
+	end process;
 	
 	-----------------------------------------------------	
 	
